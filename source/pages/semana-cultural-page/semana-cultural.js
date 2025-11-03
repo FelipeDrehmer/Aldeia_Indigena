ccfa7bb910887aa8722648f566b0e8cf6ee8f623
Felipe Drehmer <felipeka.drehmer@gmail.com>
2025-11-03 11:35:23 -0300

    Finalizado js semana cultural

--- source/pages/semana-cultural-page/semana-cultural.js
+++ source/pages/semana-cultural-page/semana-cultural.js
@@ -1,5 +1,5 @@
 async function getData() {
-    const url = 'https://hbc0k9b0.api.sanity.io/v2025-10-30/data/query/production?query=*%5B_type+%3D%3D+%22semanaCultural%22%5D%5B0%5D%7B%0A++++++++tituloGeral%2C%0A++++++++descricaoGeral%2C%0A++++++++blocos%5B%5D+%7B%0A++++++++++++titulo%2C%0A++++++++++++descricao%2C%0A++++++++++++imagem%0A++++++++%7D%0A++++%7D&perspective=';
+    const url = 'https://hbc0k9b0.api.sanity.io/v2025-11-03/data/query/production?query=*%5B_type+%3D%3D+%22semanaCultural%22%5D%5B0%5D%7B%0A++++++++tituloGeral%2C%0A++++++++descricaoGeral%2C%0A++++++++blocos%5B%5D+%7B%0A++++++++++++titulo%2C%0A++++++++++++descricao%2C%0A++++++++++++%22url%22%3A+imagem.asset-%3Eurl%2C%0A++++++++%7D%0A++++%7D&perspective=raw';
 
     try {
         const response = await fetch(url, {
@@ -22,7 +22,6 @@ async function getData() {
 
 async function montarPagina(resposta) {
     try {
-
         const tituloGeral = document.querySelector("#tituloGeral");
         if (tituloGeral && resposta.tituloGeral) {
             tituloGeral.textContent = resposta.tituloGeral;
@@ -30,20 +29,50 @@ async function montarPagina(resposta) {
 
         const descricaoTopico = document.querySelector("#descricaoGeral");
         if (descricaoTopico && resposta.descricaoGeral) {
-
             descricaoTopico.innerHTML = "";
-
             resposta.descricaoGeral.forEach((texto) => {
                 const itemTopico = document.createElement("li");
                 itemTopico.textContent = texto;
-                itemTopico.classList.add("text-3xl", "text-black", "mb-2", "leading-relaxed"); 
+                itemTopico.classList.add("text-xl", "text-black", "mb-2", "leading-relaxed", "lg:text-3xl");
                 descricaoTopico.appendChild(itemTopico);
             });
         }
 
+        const blocosDOM = document.querySelectorAll(".blocos");
+        blocosDOM.forEach((secao, index) => {
+            const blocoDados = resposta.blocos[index];
+            if (!blocoDados) return;
+
+            const titulo = secao.querySelector(".tituloBloco");
+            if (titulo) titulo.textContent = blocoDados.titulo;
+
+            const descricao = secao.querySelector(".descricaoBloco");
+            if (descricao) descricao.textContent = blocoDados.descricao;
+
+            const img = secao.querySelector(".imgBloco");
+            if (img && blocoDados.url) {
+                img.src = blocoDados.url;
+            }
+        });
+
+        const imagens = document.querySelectorAll(".imgBloco");
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    img.classList.remove("opacity-0", "-translate-x-20");
+
+                    img.classList.add("opacity-100", "translate-x-0");
+                    observer.unobserve(img); 
+                }
+            });
+        }, { threshold: 0.3 }); 
+
+        imagens.forEach(img => observer.observe(img));
+
         const params = new URLSearchParams(window.location.search);
         const alvo = params.get('scroll');
-
         if (alvo) {
             const destino = document.getElementById(alvo);
             if (destino) {
