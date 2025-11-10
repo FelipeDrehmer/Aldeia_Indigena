0140d5e7961d54d7b1b34cf8f47db4a0a7e4f7ff
Felipe Drehmer <felipeka.drehmer@gmail.com>
2025-11-10 11:20:04 -0300

    Criado cards galeria

--- source/pages/galeria-page/galeria.js
+++ source/pages/galeria-page/galeria.js
@@ -30,6 +30,30 @@ async function montarPagina(resposta) {
             descricaoGeral.textContent = resposta.textoGeral;
         }
 
+        const cardsSecao = document.querySelector('#balaoCards');
+        if (cardsSecao && Array.isArray(resposta.cards)) {
+            cardsSecao.innerHTML = ''; // limpa o container
+
+            resposta.cards.forEach((card, index) => {
+                const cor = index % 2 === 0 ? '#8B572A' : '#1c3d16'; // alterna as cores
+                
+                const divCardUnico = document.createElement('div');
+                divCardUnico.className = 'w-[270px] rounded-2xl overflow-hidden shadow-lg';
+                divCardUnico.style.backgroundColor = cor;
+
+                divCardUnico.innerHTML = `
+                    <img src="${card.foto}" alt="Imagem do card" class="w-full h-[150px] object-cover" />
+                    <div class="p-4">
+                        <p class="text-white text-sm leading-relaxed">
+                            ${card.descricao || ''}
+                        </p>
+                    </div>
+                `;
+
+                cardsSecao.appendChild(divCardUnico);
+            });
+        }
+
     } catch(err) {
         console.error('Erro ao montar página:', err);
     }
@@ -38,5 +62,6 @@ async function montarPagina(resposta) {
 
 window.addEventListener('load', async () => {
     const resposta = await getData();
+    console.log(resposta)
     if (resposta) montarPagina(resposta);
 });
